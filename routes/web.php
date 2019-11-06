9cc72cb2c34fa5407ae7e4354a804d2e4c0f427e
liu <liuweicheng2019@126.com>
2019-11-06 19:46:00 +0800

    edit self info

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
-//Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //        ||
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update'); //会出错
