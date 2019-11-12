f226be13ea90e56cb8204acdf846ba68c6894fa2
测试qnmd <liuweicheng2019@126.com>
2019-11-12 13:21:01 +0800

    show avatar

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,4 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 //        ||
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update'); //会出错
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update'); //会出错,因为patch
